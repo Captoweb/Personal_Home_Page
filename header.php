6ea9e3b0bd10885b18f70d488ce3663284f4b4a4
Cap <captainphp@yandex.ru>
2017-07-05 00:57:26 +0300

    Peredelki

--- header.php
+++ header.php
@@ -3,6 +3,6 @@
 </head>
 <div class="header">
 
-<h1 align="center">Страница на GitHub</h1>
+<h1 align="center">Personal Home Page </h1>
 <p><img src="/images/logo.jpg"></p>
 </div>
--- menu.php
+++ menu.php
@@ -1,6 +1,8 @@
 <ul>
     <li><a href="/">Главная</a></li>
-    <li><a href="/">Менее главная</a></li>
-    <li><a href="/">Не очень главная</a></li>
-    <li><a href="/">Совсем не главная</a></li>
+    <li><a href="/images/rubber.jpg""#">Резиновый сайт</a></li>
+    <li><a href="/images/database.jpg""#">Наша база данных</a></li>
+        <li><a href="/images/programmer.jpg""#">Наши программисты</a></li>
+    <li><a href="/images/career_development.jpg""#">Карьера</a></li>
+    <li><a href="/images/Git_begining.jpg""#">Git: начало</a></li>
 </ul>

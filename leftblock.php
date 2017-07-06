3156e56452ad5356a15cc64a305b2cc3f05279c4
Cap <captainphp@yandex.ru>
2017-07-07 01:22:39 +0300

    resum3

--- leftblock.php
+++ leftblock.php
@@ -3,7 +3,11 @@
     
     
     <h2>Кратко обо мне</h2>
- <p><img src="/images/skills2.jpg"></p>   
+ 
+    
+
+
+ <p><img src="/images/skills2.jpg" width="300" height="280" align="right"></p>   
    <p>Более полугода осваиваю веб-программирование, начиная с HTML, CSS, Javascript, PHP, MySQL, и продолжая Git, Github, Ubuntu, Bash, Joomla, Bootstrap. Прошел серию уроков на сайте Hexlet: Основы PHP, Введение в веб-разработку, Операционные системы, Логика, Введение в Си, PHP: Начало работы, JS: подготовка к работе, Регулярные выражения, Bash: Основы командной строки</p>
     
     

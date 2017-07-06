7975c6fe86edeeb81b510dd7ff7ee9cca82be0da
Cap <captainphp@yandex.ru>
2017-07-07 00:31:13 +0300

    resum

--- menu.php
+++ menu.php
@@ -1,8 +1,8 @@
 <ul>
     <li><a href="/">Главная</a></li>
-    <li><a href="/images/rubber.jpg""#">Резиновый сайт</a></li>
-    <li><a href="/images/database.jpg""#">Наша база данных</a></li>
-        <li><a href="/images/programmer.jpg""#">Наши программисты</a></li>
-    <li><a href="/images/career_development.jpg""#">Карьера</a></li>
+    <li><a href="https://ru.hexlet.io/my""#">Сайт по которому я готовился</a></li>
+    <li><a href="/images/books.jpg""#">Книги которые я прочитал</a></li>
+        <li><a href="/images/programmer.jpg""#">Как я отдыхаю</a></li>
+    <li><a href="/images/database.jpg""#">Самый дешевый хостинг</a></li>
     <li><a href="/images/Git_begining.jpg""#">Git: начало</a></li>
 </ul>
